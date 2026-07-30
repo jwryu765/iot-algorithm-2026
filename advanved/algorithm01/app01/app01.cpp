961bb803a69c82c214c058abcff2faed0158f814
jwryu765 <jhryu7658@gmail.com>
2026-07-30 15:11:58 +0900

    알고리즘 고급

---
+++ advanved/algorithm01/app01/app01.cpp
@@ -0,0 +1,105 @@
+﻿// app01.cpp : 빌트인 데이터타입
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a = 13;
+    int b = 4;
+
+    cout << "정수 연산" << endl;
+    // 일반 연산
+    cout << a + b << endl; // 17
+    cout << a - b << endl; // 9
+    cout << a * b << endl; // 52
+    cout << a / b << endl; // 3
+    cout << a % b << endl; // 1
+    cout << -a << endl; // -13
+
+    // 비교 연산
+    cout << (a == b) << endl; // 0 false
+    cout << (a != b) << endl; // 1 true
+    cout << (a > b) << endl;  // 1
+    cout << (a < b) << endl; // 0
+    cout << (a >= b) << endl;  // 1
+    cout << (a <= b) << endl;  // 0
+
+    // 비트 연산
+    cout << (a & b) << endl; // 13 -> 1101     4 ->  0100  ==>   0100(4)
+    cout << (a | b) << endl;  // 1101 | 0100 -> 1101(13)
+
+    cout << "부동소수 연산" << endl;
+    double d = 2.5;
+    float f = 1.5f;
+
+    cout << sizeof(d) << " byte" << endl; // 8 byte
+    cout << sizeof(f) << " byte" << endl; // 4 byte
+
+    cout << d << " " << f << endl;  // 2.5 1.5
+    cout << d + f << endl;  // 4
+    cout << d - f << endl;  // 1
+    cout << d * f << endl;  // 3.75
+    cout << d / f << endl;  // 1.66667
+
+    // 형변환
+    cout << "형 변환" << endl;
+
+    int i = 65;
+    f = 5.2f;
+
+    // 암시적 형변환(메모리가 큰 변수로 변경)
+    d = i + f;   // i + f (일단 float 변경) -> double 할당(double 변경)
+    cout << d << endl;  // 70.2
+    // 명시적 형변환 double -> int
+    cout << static_cast<int>(d - 5) << endl; // 65
+    // 명시적 형변환 int -> char
+    cout << static_cast<char>(i) << endl; // 'A'
+
+    // 문자열
+    cout << "문자열" << endl;
+
+    string str1;     // 빈 문자열
+    string str2 = "Hello, C++!"; // 문자열 초기화
+    string str3(str2);  // 문자열 복사
+    string str4(str2, 0, 5);  // Hello 복사
+    string str5(10, '*');   // **********
+
+    // 문자열 찾기
+    cout << "문자열 찾기" << endl;
+
+    string str = "Hello, C++ World!";
+
+    // Hello 문자열 찾기
+    size_t pos1 = str.find("Hello");
+    cout << pos1 << endl;   // 0
+
+    // 'C' char 찾기
+    size_t pos2 = str.find('C');
+    cout << pos2 << endl;   // 7
+
+    // 시작 인덱스 지정하고 Hello 조회
+    size_t start_index = 2;  // l 부터 시작해서 찾기
+    size_t pos3 = str.find("Hello", start_index);
+    cout << pos3 << endl;    // 18446744073709551615   -> 인덱스를 찾을 수 없음
+
+    // 존재하지 않는 문자열 조회
+    size_t pos4 = str.find("Python");
+    cout << pos4 << endl;  // 18446744073709551615  -> Python은 문자열에 없음
+
+    // 문자열 추가, 수정
+    str = "APPLE";
+    str += ", World!";   // 문자열 추가
+    cout << str << endl;     // APPLE, World!
+
+    str[7] = 'P'; // W -> P
+    cout << str << endl;     // APPLE, Porld!
+
+    str.replace(7, 4, "Col");  // 7번째 인덱스 글자부터 4글자를 Col로 변경
+    //str.replace(7, 3, "Co");  // 7번째 인덱스 글자부터 3글자를 Co로 변경
+    cout << str << endl;     // APPLE, Cold!
+
+    return 0;
+}
---
+++ advanved/algorithm01/app02/app02.cpp
@@ -0,0 +1,123 @@
+﻿// app02.cpp : STL
+//
+
+#include <iostream>
+// 2. 필요 프리프로세서 추가
+#include <vector>
+#include <map>
+#include <set>
+#include <string>
+// 4. find 함수의 헤더
+#include <algorithm>
+
+using namespace std;
+
+void modifyByValue(int value) {
+    value = 10;
+    cout << "2. 주소 : " << &value << endl;  // 00000072495EEC00
+    cout << "2. 값   : " << value << endl;  // 10
+
+    // 함수가 종료되면 value는 메모리에서 사라짐!
+}
+
+void modifyByReference(int& value) {
+    value = 15;
+    cout << "4. 주소 : " << &value << endl;  // 00000072495EEC24
+    cout << "4. 값   : " << value << endl;  // 15
+}
+
+int main()
+{
+    // 1. 상수 레퍼런스
+    // `Call by Value` vs `Call by reference`
+    // 컨테이너 객체, 구조체를 Call by Value로 넘기면 성능에 지대한 영향
+    int value = 5;
+    cout << "1. 주소 : " << &value << endl;  // 00000072495EEC24
+    cout << "1. 값   : " << value << endl;  // 5
+
+    modifyByValue(value);  // 함수 호출
+    cout << "3. 주소 : " << &value << endl;  // 00000072495EEC24
+    cout << "3. 값   : " << value << endl;  // 5
+
+    modifyByReference(value);  // 
+    cout << "5. 주소 : " << &value << endl;  // 00000072495EEC24
+    cout << "5. 값   : " << value << endl;  // 15
+
+    // 2. auto문  -> javascript, C#의 var 와 동일
+    auto num = 42;   // int로 추론
+    cout << num << endl;
+
+    auto pi = 3.141592;  // double로 추론
+    cout << pi << endl;
+
+    auto greeting = string("Hello, C++!");  // string으로 추론
+    cout << greeting << endl;
+
+    // 3. 범위기반 반복문 foreach, for in
+    // 컨테이너 등에서 모든 원소를 반복적 순회
+
+    // vector : 1차원 배열
+    cout << "벡터 " << endl;
+    vector<int> vec = { 1,2,3,4,5 };
+    for (const auto& num : vec) {  // for (int num : vec) {
+        cout << num << ". ";
+    }
+    cout << endl;
+
+    // map : Dictionary
+    cout << "맵 " << endl;
+    map<string, int> fruitMap = { {"apple", 1}, {"banana", 2}, {"mango", 3}, {"cherry", 4} };
+    for (const auto& pair : fruitMap) { // for (auto pair : fruitMap) {
+        cout << pair.first << " => " << pair.second << ". ";
+    }
+    cout << endl;
+
+    // set : 집합
+    cout << "집합 " << endl;
+    set<string> fruitSet = { "apple", "banana", "mango", "cherry", "banana" };
+    for (const auto& fruit : fruitSet) {  // for (string fruit : fruitSet) {
+        cout << fruit << ". ";
+    }
+    cout << endl;
+
+    // 4. 반복자 - vector, map, set 등 컨테이너를 종류와 관계없이 원소 순회 접근가능하도록 처리
+    cout << "반복자 " << endl;
+    vector<int> vec2 = { 10,20,30,40,50,60,70,80,90,100 };
+
+    // 반복자 순회
+    for (auto it = vec2.begin(); it != vec2.end(); ++it) {
+        cout << *it << ". ";
+    }
+    cout << endl;
+
+    // 탐색
+    auto result = find(vec2.begin(), vec2.end(), 40);
+    if (result != vec2.end()) {
+        cout << "Found : " << *result << endl; // Found : 40
+    }
+    else {
+        cout << "Not found." << endl;
+    }
+
+    // 역방향 반복자
+    for (auto it = vec2.rbegin(); it != vec2.rend(); ++it) {
+        cout << *it << ". ";
+    }
+    cout << endl;
+
+    // fruitMap 순회
+    for (auto it = fruitMap.begin(); it != fruitMap.end(); ++it) {
+        cout << it->first << ": " << it->second << endl;
+    }
+
+    // 원소 탐색
+    auto result2 = fruitMap.find("mango");
+    if (result2 != fruitMap.end()) {
+        cout << "Found : " << result2->first << "=> " << result2->second << endl;
+    }
+    else {
+        cout << "Not found." << endl;
+    }
+
+    return 0;
+}
